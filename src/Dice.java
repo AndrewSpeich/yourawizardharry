00272f236277d9d344b877841eb111a75dcb8274
IanRJacobs <jacobia@wi.rr.com>
2016-03-08 15:22:06 -0600

    resync2

--- src/Dice.java
+++ src/Dice.java
@@ -1,8 +1,21 @@
 
 public class Dice {
 
-	public void rollDice(int sides){
-		int roll;
-		roll = (int) Math.round(Math.random()*(sides-1))+1;
+	public int RollResult(int sidesNumber)
+	{
+		int rollResult = (int) Math.round(Math.random()
+				* (sidesNumber - 1)) + 1;
+		return rollResult;
 	}
+
+	public int RollMultiple(int sidesNumber,int amountOfDice)
+	 	{
+	 		int reRollResult = 0;
+	 		
+	 		for(int count = 0; count < amountOfDice; count++)
+	 		{
+	 			 reRollResult += RollResult(sidesNumber);
+	 	}
+	 		return reRollResult;
+	 	}
 }
